26b3a400149fa8fa4a3ccb4c9fa53b19312234bb
Stozo <35351828+Stozo@users.noreply.github.com>
2024-09-23 22:05:29 +0200

    Oppgave 6.py Done

---
+++ Oppgave 6.py
@@ -0,0 +1,25 @@
+packing_list = []
+while True:
+    print("Packing List")
+    print("1. Add item to list")
+    print("2. Remove item from list")
+    print("3. Print List")
+    print("4. Exit")
+    user_input = input("Write the corresponding number to execute a command\n\n> ")
+    if user_input == "1":
+        item_to_be_added =str(input("Enter the item to add\n\n> "))
+        packing_list.append(item_to_be_added)
+        print(f"This is currently your list:{packing_list}")
+    elif user_input == "2":
+        item_to_remove = str(input("Enter the item to remove\n\n> "))
+        if item_to_remove in packing_list:
+            packing_list.remove(item_to_remove)
+            print(f"This is currently your list:{packing_list}")
+        else:
+            print("This item is not in the list\n")
+    elif user_input == "3":
+        print(f"This is currently your list:{packing_list}")
+    elif user_input == "4":
+        break
+    else:
+        print("Invalid input, please try again\n")
